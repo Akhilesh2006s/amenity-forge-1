aa8f52418862323b3d8fbe0f86ff0bed1d5c5299
Akhilesh2006s <155376342+Akhilesh2006s@users.noreply.github.com>
2025-07-02 10:34:34 +0530

    Update page.tsx

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,21 +1,18 @@
+// app/brochure/page.tsx
 "use client"
 
-import HeroGeometric from "@/components/Amenity forge/hero-geometric"
-import Service from "@/components/Amenity forge/Services"
-import Choose from "@/components/Amenity forge/Choose"
-import Project from "@/components/Amenity forge/Projects"
-import Result from "@/components/Amenity forge/Results"
+import Home from "@/components/Amenity forge/Home"
 import Navbar from "@/components/Amenity forge/navbar"
+import Service from "@/components/Amenity forge/Service"
 
-export default function Page() {
+
+export default function BrochurePage() {
   return (
-    <>
-    <Navbar />
-    <HeroGeometric />
-    <Service/>
-    <Choose/>
-    <Project/>
-    <Result/>
-    </>
-  );
+    <section id="about" className="bg-black">
+          <Navbar />
+      
+      <Home />
+      <Service/>
+    </section>
+  )
 }
