41c7a9cdd2ef1cf880bd462467f066ab83d3a3e4
Akhilesh2006s <155376342+Akhilesh2006s@users.noreply.github.com>
2025-07-02 12:12:02 +0530

    Update navbar.tsx

--- components/Amenity forge/navbar.tsx
+++ components/Amenity forge/navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
             className="flex items-center"
           >
             <img
-              src="/Logo.png"
+              src="/logo1.jpg"
               alt="Logo"
               className="h-10 w-auto object-contain"
             />
