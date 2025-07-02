21bd1675486af8cda1ad8827c258292a4e7e7c11
Akhilesh2006s <155376342+Akhilesh2006s@users.noreply.github.com>
2025-07-02 12:12:40 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Amenityforge',
+  description: 'Amenityforge',
+  generator: 'Amenityforge',
 }
 
 export default function RootLayout({
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo1.jpg" type="image/png" />
+      </head>
       <body>{children}</body>
     </html>
   )
